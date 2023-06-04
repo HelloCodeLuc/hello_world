30f212657c7f0ebc719071740d63d82637e967bc
HelloCodeLuc <Lucasjchan01@gmail.com>
2023-06-04 19:13:23 -0400

    Simple code to add integers

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-print("Hello World")
-print(" new line")
+print("Hi, what would you like to add today?")
+X = (input( 'Enter number X:'))
+Y = (input( 'Enter number Y:'))
+
+sum = float(X) + float(Y)
+print('Addition of X and Y is:', sum)
