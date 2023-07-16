892f10b301aa8dcd7b7a57b22bb4d5ca885dc796
HelloCodeLuc <112012277+HelloCodeLuc@users.noreply.github.com>
2023-07-16 14:03:14 -0400

    Calculator expanded to support presidency over brackets etc...

--- test.py
+++ test.py
@@ -1,6 +1,53 @@
-print("Hi, what would you like to add today?")
-X = (input( 'Enter number X:'))
-Y = (input( 'Enter number Y:'))
+import re
 
-sum = float(X) + float(Y)
-print('Addition of X and Y is:', sum)
+def calculate(expression):
+
+    pattern = r'\(([^()]+)\)'
+
+
+    def evaluate_brackets(expr):
+        match = re.search(pattern, expr)
+        while match:
+            inner_expr = match.group(1)
+            inner_result = evaluate_expression(inner_expr)
+            expr = expr.replace(match.group(), str(inner_result))
+            match = re.search(pattern, expr)
+        return evaluate_expression(expr)
+
+
+    def evaluate_expression(expr):
+
+        expr_pattern = r'\s*([-+]?\d+(?:\.\d+)?)\s*([+\-*/])\s*([-+]?\d+(?:\.\d+)?)\s*$'
+        match = re.match(expr_pattern, expr)
+        if match:
+            num1 = float(match.group(1))
+            operator = match.group(2)
+            num2 = float(match.group(3))
+
+            if operator == '+':
+                return num1 + num2
+            elif operator == '-':
+                return num1 - num2
+            elif operator == '*':
+                return num1 * num2
+            elif operator == '/':
+                if num2 != 0:
+                    return num1 / num2
+                else:
+                    return "Error: Division by zero"
+        else:
+            return "Error: Invalid expression"
+
+
+    result = evaluate_brackets(expression)
+
+    return result
+
+
+while True:
+    expression = input("Enter a mathematical expression (or 'q' to quit): ")
+    if expression.lower() == 'q':
+        break
+
+    result = calculate(expression)
+    print("Result:", result)
